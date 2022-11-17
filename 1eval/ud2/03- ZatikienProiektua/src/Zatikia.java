bdb8f983208b5f1a053909ea630810affd7d15c7
Alberdi Arrillaga <alberdi.jon@uni.lan>
2022-11-17 09:58:19 +0100

    17/11

--- 1eval/ud2/03- ZatikienProiektua/src/Zatikia.java
+++ 1eval/ud2/03- ZatikienProiektua/src/Zatikia.java
@@ -28,4 +28,25 @@ public class Zatikia {
         return "Zatikia[" + zenbakitzailea + "/" + izendatzailea + "]";
     }
 
+    public Zatikia biderkatu( Zatikia zat1, Zatikia zat2){
+
+        int zenb, izend;
+
+        zenb =  zat1.zenbakitzailea * zat2.zenbakitzailea;
+        izend =  zat1.izendatzailea * zat2.izendatzailea;
+
+        Zatikia zat3 = new Zatikia(zenb, izend);
+
+        return zat3;
+
+    }
+
+    public static Zatikia batu(Zatikia zat1, Zatikia zat2) {
+
+        Zatikia batuketa;
+
+        
+
+    }
+
 }
