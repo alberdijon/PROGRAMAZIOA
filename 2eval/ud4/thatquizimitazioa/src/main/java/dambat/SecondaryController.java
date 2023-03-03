daf78949a27f28c4e76a88acc2920e9c4faa09c5
alberdijon <alberdi.jon@uni.eus>
2023-03-03 09:58:02 +0100

    03/03

--- 2eval/ud4/thatquizimitazioa/src/main/java/dambat/SecondaryController.java
+++ 2eval/ud4/thatquizimitazioa/src/main/java/dambat/SecondaryController.java
@@ -6,7 +6,7 @@ import javafx.fxml.FXML;
 public class SecondaryController {
 
     @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+    private void switchToFinal() throws IOException {
+        App.setRoot("final");
     }
 }
